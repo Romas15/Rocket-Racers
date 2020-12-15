fa05a161184356326da769bd72d2aed7ecf9cae5
Romas15 <71475591+Romas15@users.noreply.github.com>
2020-12-15 13:54:14 -0600

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,7 +1,7 @@
 class Form {
   constructor() {
     this.button = createButton("Play");
-
+   this.reset = createButton("Reset");
     this.title = createElement("h1");
   }
 
@@ -15,6 +15,13 @@ class Form {
     this.title.position(displayWidth / 2 - 120, 330);
 
     this.button.position(displayWidth / 2 - 50, displayHeight / 2 - 50);
+    
+     this.reset.position(displayWidth / 2 - 700, displayHeight / 2 - 400);
+
+    this.reset.mousePressed(() => {
+      game.update(0);
+      player.updateCount(0);
+    });
 
     this.button.mousePressed(() => {
       this.button.hide();
